27bbb0b4810f22b257bbb06c99f77452046e3363
Dineshdk555 <104187302+Dineshdk555@users.noreply.github.com>
2022-05-16 20:29:26 +0530

    login ui updated

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import MetaTags from 'react-meta-tags';
+import Breadcrumbs from "components/Common/Breadcrumb";
+import {
+  Container,
+} from "reactstrap"
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="page-content">
+          <MetaTags>
+            <title>Dashboard | Rain - Admin & Dashboard Template</title>
+          </MetaTags>
+          <Container fluid>
+          <Breadcrumbs title="Cases" breadcrumbItem="Case Grid" />
+          </Container>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Dashboard;
